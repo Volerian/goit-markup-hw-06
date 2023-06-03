2226b2c6949012f344a79f1ca93c416d50d4860b
Volerian <mmm271003@gmail.com>
2023-06-03 14:02:51 +0300

    fixed all files

--- js/modal.js
+++ js/modal.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 (() => {
   const refs = {
     openModalBtn: document.querySelector("[data-modal-open]"),
@@ -12,19 +11,4 @@
   function toggleModal() {
     refs.modal.classList.toggle("is-hidden");
   }
-=======
-(() => {
-  const refs = {
-    openModalBtn: document.querySelector("[data-modal-open]"),
-    closeModalBtn: document.querySelector("[data-modal-close]"),
-    modal: document.querySelector("[data-modal]"),
-  };
-
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
-  function toggleModal() {
-    refs.modal.classList.toggle("is-hidden");
-  }
->>>>>>> 4434b0e23024efdf07b75c92ed7e93175707b108
 })();
